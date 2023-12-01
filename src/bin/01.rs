797f4f8905b70449479d082095bf9fe11ffa2152
Adrian Wowk <dev@adrianwowk.com>
2023-12-01 18:50:36 -0500

    reorganize project structure
    Added custom day template with macro that automatically generates tests
    and benchmarks for you.

---
+++ src/bin/01.rs
@@ -0,0 +1,59 @@
+#![feature(test)]
+
+advent_of_code_2023::solution!(1);
+
+use rayon::prelude::*;
+
+#[inline]
+pub fn part_1(input: &str) -> Option<u32> {
+    Some(input.lines().map(extract_calibration_value_1).sum())
+}
+
+#[inline]
+pub fn part_2(input: &str) -> Option<u32> {
+    Some(input.par_lines().map(extract_calibration_value_2).sum())
+}
+
+pub fn extract_calibration_value_1(line: &str) -> u32 {
+    let mut digits = line
+        .bytes()
+        .filter(|c| c.is_ascii_digit())
+        .peekable();
+
+    let first = *digits.peek().unwrap();
+    let last = digits.last().unwrap();
+
+    (first - b'0') as u32 * 10 + (last - b'0') as u32
+}
+
+pub fn extract_calibration_value_2(line: &str) -> u32 {
+    let chars: Vec<_> = line.chars().collect();
+
+    let mut digits = Vec::new();
+
+    for i in -4..chars.len() as isize {
+        let min = i.clamp(0, chars.len() as isize);
+        let max = (i + 5).clamp(0, chars.len() as isize);
+        let window = &chars[min as usize..max as usize];
+
+        let digit = match window {
+            ['0'..='9', ..] => window[0].to_digit(10),
+            ['o', 'n', 'e', ..] => Some(1),
+            ['t', 'w', 'o', ..] => Some(2),
+            ['t', 'h', 'r', 'e', 'e'] => Some(3),
+            ['f', 'o', 'u', 'r', ..] => Some(4),
+            ['f', 'i', 'v', 'e', ..] => Some(5),
+            ['s', 'i', 'x', ..] => Some(6),
+            ['s', 'e', 'v', 'e', 'n'] => Some(7),
+            ['e', 'i', 'g', 'h', 't'] => Some(8),
+            ['n', 'i', 'n', 'e', ..] => Some(9),
+            _ => None,
+        };
+
+        if let Some(digit) = digit {
+            digits.push(digit);
+        }
+    }
+
+    *digits.first().unwrap() * 10 + *digits.last().unwrap()
+}
---
+++ src/lib.rs
@@ -0,0 +1,82 @@
+#[macro_export]
+macro_rules! solution {
+    ($day:expr) => {
+        extern crate test;
+
+        use advent_of_code_2023::{read_example_file, read_input_file, Part};
+
+        const DAY: u32 = $day;
+
+        fn main() {
+            let input = read_input_file(DAY);
+            println!("Part 1: {:?}", part_1(&input));
+            println!("Part 2: {:?}", part_2(&input));
+        }
+
+        #[cfg(test)]
+        mod tests {
+            use test::{black_box, Bencher};
+
+            use super::*;
+
+            #[test]
+            fn part_1_example() {
+                let (expected_result, input) = read_example_file(DAY, Part::One);
+                let result = part_1(&input);
+
+                assert_eq!(result, Some(expected_result));
+            }
+
+            #[test]
+            fn part_2_example() {
+                let (expected_result, input) = read_example_file(DAY, Part::Two);
+                let result = part_2(&input);
+
+                assert_eq!(result, Some(expected_result));
+            }
+
+            #[bench]
+            fn bench_part_1(b: &mut test::Bencher) {
+                let input = read_input_file(DAY);
+                b.iter(|| black_box(part_1(&input)));
+            }
+
+            #[bench]
+            fn bench_part_2(b: &mut test::Bencher) {
+                let input = read_input_file(DAY);
+                b.iter(|| black_box(part_2(&input)));
+            }
+        }
+    };
+}
+
+#[derive(Debug, Clone, Copy)]
+#[repr(u8)]
+pub enum Part {
+    One = 1,
+    Two = 2,
+}
+
+pub fn read_input_file(day: u32) -> String {
+    let path = format!("data/inputs/{:02}.txt", day);
+    std::fs::read_to_string(path).unwrap()
+}
+
+pub fn read_example_file(day: u32, part: Part) -> (u32, String) {
+    let path = format!("data/examples/{:02}/part-{:01}.txt", day, part as u8);
+    let file = std::fs::read_to_string(&path)
+        .unwrap_or_else(|_| panic!("Example file not found: {}", path));
+
+    let (expected_result, input) = file
+        .split_once("\n===\n")
+        .expect("Example file is not in the correct format");
+
+    let expected_result = expected_result
+        .trim()
+        .parse::<u32>()
+        .expect("Expected result is not a number");
+
+    let input = input.trim().to_string();
+
+    (expected_result, input)
+}
